70c4a0dffab70d3e189ae27cf329cc405caa800e
Eric Kim <hyeokjungkim@gmail.com>
2021-03-21 02:02:06 -0400

    seed and destroy

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,7 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :classroom
   has_attached_file :pdf
-  has_many :grades,-> { order(:id) }, dependent: :delete_all
+  has_many :grades,-> { order(:id) }, dependent: :destroy
 
   validates :description, presence: true, length: { maximum: 120 }
   validates_attachment_content_type :pdf, :content_type =>['application/pdf']
