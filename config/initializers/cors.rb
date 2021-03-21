56024eec002ccf30dc88b46385e3bd999dba2ae2
Eric Kim <hyeokjungkim@gmail.com>
2021-03-20 21:47:49 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://whiteboard-gradebook.netlify.app/'
+    origins ['https://whiteboard-gradebook.netlify.app', "http://localhost:3001"]
 
     resource '*',
       headers: :any,
